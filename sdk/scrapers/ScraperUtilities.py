b8a35b42da5adb313676e65b55d9ac36bf0c850e
Anderson Tseng <tseng.andersonn@gmail.com>
2025-11-10 23:39:15 -0800

    Refactor createSession to remove caching logic

--- sdk/scrapers/ScraperUtilities.py
+++ sdk/scrapers/ScraperUtilities.py
@@ -3,15 +3,18 @@ import requests_cache
 
 
 
-def createSession(db_location:str, use_cache=False) -> requests_cache.CachedSession | requests.Session:
-    if use_cache:
-        return requests_cache.CachedSession(
+def createSession(db_location:str, use_cache=False) -> requests.Session:
+        return requests.Session()
+
+# def createSession(db_location:str, use_cache=False) -> requests_cache.CachedSession | requests.Session:
+    # if use_cache:
+    #     return requests_cache.CachedSession(
             # "database/cache",# CACHE_LOCATION, 
             # backend="filesystem",
-            db_location, 
-            backend="sqlite",
-            allowable_methods=("GET", "POST"),
-            ignored_parameters=["_wpnonce"]
-        )
-    else:
-        return requests.Session()
+    #         db_location, 
+    #         backend="sqlite",
+    #         allowable_methods=("GET", "POST"),
+    #         ignored_parameters=["_wpnonce"]
+    #     )
+    # else:
+        # return requests.Session()
